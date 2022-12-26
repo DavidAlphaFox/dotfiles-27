4c1aacd76dd92b35d2bde5217c8b50cb544512a8
shved <shvedkuzmin2@gmail.com>
2022-12-26 15:11:24 +0100

    Updated colors

--- .config/bspwm/colors/gruvbox-dark.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# gruvbox 
-# https://github.com/shvedes/dotfiles
-
-bspc config normal_border_color        \#7c6f64     # border color of unfocused window
-bspc config focused_border_color       \#a04c42     # border color og focused window
-bspc config active_border_color        \#a04c42     # border color og active window
